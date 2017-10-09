153864d96a14f2ff33a4c7e0989dc69da9565963
home pig <fengchaoxhao@qq.com>
2017-10-09 17:28:48 +0800

    入门程序

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,12 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Date;
+
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        Logger logger = LoggerFactory.getLogger(HelloWorld.class);
+        //{}占位符
+        logger.info("Hello World {}",new Date());
     }
 }
